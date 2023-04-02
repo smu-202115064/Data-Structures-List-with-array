91bfa1e273e8d1eb53ba7f6afc94c5177c7b6312
Hepheir <hepheir@gmail.com>
2023-04-02 20:45:58 +0900

    feat: linear search를 이용하여 과제 구현

--- smu-HAEA9241-Data-Structures-List-with-array/main.cpp
+++ smu-HAEA9241-Data-Structures-List-with-array/main.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -26,73 +28,152 @@ public:
 	int weight;
 };
 
-Student* studentList;
-int studentCount;
 
-void createStudents() {
-	studentList = new Student[MAX_STUDENTS];
-	studentCount = 0;
-}
+class StudentList {
+private:
+	Student* arr = nullptr;
+	int size = 0;
+	int cnt = 0;
 
-void loadStudents(const char fileName[]) {
-	FILE* fp = fopen(fileName, "r");
-	if (fp == NULL) {
-		printf("Failed to open file %s.\n", fileName);
-		return;
+	void swap(Student& s1, Student& s2) {
+		Student tmp = s1;
+		s1 = s2;
+		s2 = tmp;
 	}
-	while (feof(fp) == 0) {
-		fscanf(fp, "%s %c %s %s %f %d %d",
-			studentList[studentCount].name,
-			&studentList[studentCount].gender,
-			studentList[studentCount].location,
-			studentList[studentCount].department,
-			&studentList[studentCount].gpa,
-			&studentList[studentCount].height,
-			&studentList[studentCount].weight);
-		studentCount++;
-	}
-	fclose(fp);
-	return;
-}
 
-void printStudents() {
-	for (int i = 0; i < studentCount; i++) {
+	bool cmp(const Student& s1, const Student& s2) {
+		return strcmp(s1.name, s2.name) > 0;
+	}
+
+	void bubbleSort() {
+		for (int i = 0; i < cnt - 1; i++) {
+			for (int j = i; j < cnt - 1 - i; j++) {
+				if (cmp(arr[j], arr[j+1])) {
+					swap(arr[j], arr[j+1]);
+				}
+			}
+		}
+	}
+
+	void print(const Student& s) {
 		printf("%s %c %s %s %.2f %d %d\n",
-			studentList[i].name,
-			studentList[i].gender,
-			studentList[i].location,
-			studentList[i].department,
-			studentList[i].gpa,
-			studentList[i].height,
-			studentList[i].weight);
+			s.name,
+			s.gender,
+			s.location,
+			s.department,
+			s.gpa,
+			s.height,
+			s.weight);
 	}
-}
 
-void insertStudent(
-	const char* name,
-	const char* gender,
-	const char* location,
-	const char* department,
-	const char* gpa,
-	const char* height,
-	const char* weight
-) {
-	strcpy(studentList[studentCount].name, name);
-	studentList[studentCount].gender = gender[0];
-	strcpy(studentList[studentCount].location, location);
-	strcpy(studentList[studentCount].department, department);
-	studentList[studentCount].gpa = atof(gpa);
-	studentList[studentCount].height = atoi(height);
-	studentList[studentCount].weight = atoi(weight);
-	studentCount++;
-}
+	int linearSearch(const char* name) {
+		for (int i = 0; i < cnt; i++) {
+			if (strcmp(arr[i].name, name) == 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
-void deleteStudent(const char* name) {
 
-}
+public:
+	void create() {
+		size = MAX_STUDENTS;
+		arr = new Student[size];
+		cnt = 0;
+	}
+
+	void load(const char fileName[]) {
+		FILE* fp = fopen(fileName, "r");
+		if (fp == NULL) {
+			printf("Failed to open file %s.\n", fileName);
+			return;
+		}
+		while (feof(fp) == 0) {
+			fscanf(fp, "%s %c %s %s %f %d %d",
+				arr[cnt].name,
+				&arr[cnt].gender,
+				arr[cnt].location,
+				arr[cnt].department,
+				&arr[cnt].gpa,
+				&arr[cnt].height,
+				&arr[cnt].weight);
+			cnt++;
+		}
+		fclose(fp);
+		bubbleSort();
+	}
+
+	void print() {
+		for (int i = 0; i < cnt; i++) {
+			print(arr[i]);
+		}
+	}
+
+	void insert(
+		const char* name,
+		const char* gender,
+		const char* location,
+		const char* department,
+		const char* gpa,
+		const char* height,
+		const char* weight
+	) {
+		if (cnt >= size) {
+			printf("Cannot add student, the list is full.\n");
+			return;
+		}
+		Student newStudent;
+		strcpy(newStudent.name, name);
+		newStudent.gender = gender[0];
+		strcpy(newStudent.location, location);
+		strcpy(newStudent.department, department);
+		newStudent.gpa = atof(gpa);
+		newStudent.height = atoi(height);
+		newStudent.weight = atoi(weight);
+		// »ðÀÔ À§Ä¡ °áÁ¤
+		int insertBefore = 0;
+		while (insertBefore < cnt) {
+			if (!cmp(newStudent, arr[insertBefore])) {
+				break;
+			}
+			insertBefore++;
+		}
+		// »ðÀÔ °ø°£ È®º¸
+		for (int i = cnt; i > insertBefore; i--) {
+			swap(arr[i - 1], arr[i]);
+		}
+		// »ðÀÔ
+		arr[insertBefore] = newStudent;
+		cnt++;
+	}
+
+	void remove(const char* name) {
+		int index = linearSearch(name);
+		if (index == -1) {
+			printf("Student with name %s is not found.\n", name);
+			return;
+		}
+		for (int i = index; i < cnt; i++) {
+			swap(arr[i], arr[i + 1]);
+		}
+		cnt--;
+	}
+
+	void search(const char* name) {
+		int index = linearSearch(name);
+		if (index == -1) {
+			printf("Student with name %s is not found.\n", name);
+			return;
+		}
+		print(arr[index]);
+	}
+};
+
 
 int main() {
 	FILE* inputFile = fopen("input.txt", "r");
+	StudentList list;
 
 	char input[MAX_CMD_LEN];
 	char cmd[MAX_STR_LEN];
@@ -109,22 +190,22 @@ int main() {
 			param[5],
 			param[6]);
 		if (strcmp(cmd, CMD_CREATE) == 0) {
-			createStudents();
+			list.create();
 		}
 		else if (strcmp(cmd, CMD_LOAD) == 0) {
-			loadStudents(param[0]);
+			list.load(param[0]);
 		}
 		else if (strcmp(cmd, CMD_PRINT) == 0) {
-			printStudents();
+			list.print();
 		}
 		else if (strcmp(cmd, CMD_INSERT) == 0) {
-			insertStudent(param[0], param[1], param[2], param[3], param[4], param[5], param[6]);
+			list.insert(param[0], param[1], param[2], param[3], param[4], param[5], param[6]);
 		}
 		else if (strcmp(cmd, CMD_DELETE) == 0) {
-			
+			list.remove(param[0]);
 		}
 		else if (strcmp(cmd, CMD_SEARCH) == 0) {
-
+			list.search(param[0]);
 		}
 		else {
 			printf("%s is not a keyword.\n", cmd);
@@ -132,14 +213,6 @@ int main() {
 		}
 		printf("%s done ========================================\n\n", cmd);
 	}
-
-
-	int studentCount = countStudents(listFile);
-	Student* studentList = new Student[studentCount + MAX_CREATE_STUDENTS];
-
-	loadStudents(listFile, studentCount, studentList);
-
-
-	fclose(listFile);
 	fclose(inputFile);
+	return 0;
 }
